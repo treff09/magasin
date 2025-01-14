58e5c7d36ac70bb40924e304fe744e4c30271a42
treff <boundaonestephane482@gmail.com>
2025-01-14 19:00:40 +0000

    mise a jours jour deploiements partielle

--- magazin_piece/settings.py
+++ magazin_piece/settings.py
@@ -68,12 +68,12 @@ WSGI_APPLICATION = 'magazin_piece.wsgi.application'
 # # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     'default': {
